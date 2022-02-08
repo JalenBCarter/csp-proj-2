2617320b17d4414883e60f5fc3662830f0b16e3c
s3anmorrow <s3anmorrow@gmail.com>
2022-02-08 09:01:14 -0400

    add main method

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 // importing the sass stylesheet for bundling
 import "./../sass/styles.scss";
 
-// JS content goes here
-// ...
+// ----------------------------------------------- main method
+function main() {
+
+}
+
+main();
